b849dbdfbf4d6e7a9c39269f4bcae049da92fe40
Rakesh Kumar <rakeshkumaraur456@gmail.com>
2025-09-05 15:13:04 +0530

    Delete backend directory

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: String, // hashed
-  role: { type: String, default: "user" }
-});
-
-module.exports = mongoose.model("User", userSchema);
--- backend/routes/posts.js
+++
@@ -1,8 +0,0 @@
-function requireAuth(req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).send("Auth required");
-  try {
-    req.user = jwt.verify(token, process.env.JWT_SECRET || "secret");
-    next();
-  } catch { res.status(401).send("Invalid token"); }
-}
